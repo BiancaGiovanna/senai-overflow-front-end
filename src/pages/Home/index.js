c04c76cec82439ca5729244d80ebea6278607b76
BiancaGiovanna <biancagothicg@gmail.com>
2021-02-03 17:00:33 -0300

    add respostas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,7 @@ import imgProfile from "../../assets/foto_perfil.png";
 import logo from "../../assets/logo.png";
 import { Link, useHistory } from "react-router-dom";
 import { api } from "../../services/api";
-import { singOut } from "../../services/security";
+import { singOut, getUser } from "../../services/security";
 
 function Profile(params) {
   return (
@@ -39,34 +39,89 @@ function Profile(params) {
     </>
   );
 }
-
+function Answer({ answer }) {
+  return (
+    <section>
+      <header>
+        <img src={imgProfile} alt="imageProfile" />
+        <strong>por {answer.Student.name}</strong>
+      </header>
+      <p>{answer.description}</p>
+    </section>
+  );
+}
 function Question({ question }) {
+  const [showAnswers, setShowAnswers] = useState(false);
+
+  const [newAnswer, setNewAnswer] = useState(" ");
+
+  const [answers, setAnswer] = useState(question.Answers);
+
+  const qtdAnswers = question.Answers.length;
+
+  const handleAddAnswer = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post(`/questions/${question.id}/answers`, {
+        description: newAnswer,
+      });
+      const aluno = getUser();
+      const answerAdded = {
+        id: response.data.id,
+        description: newAnswer,
+        created_at: response.data.createdAt,
+        Student:{
+          id: aluno.studentId,
+          name:aluno.name,
+        }
+      } 
+
+      setAnswer([...answers, answerAdded]);
+
+      setNewAnswer(" ");
+
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <QuestionCard>
       <header>
         <img src={imgProfile} alt="profileImage" />
         <strong>Por {question.Student.name}</strong>
-        <p>em 12/12/2012 as 12:12</p>
+        <p>em {question.created_at}</p>
       </header>
       <section>
-        <strong>Titulo</strong>
-        <p>Descrição</p>
-        <img
-          src="https://blog-geek-midia.s3.amazonaws.com/wp-content/uploads/2020/11/27155450/aprender-react.png"
-          alt="postImage"
-        />
+        <strong>{question.title}</strong>
+        <p>{question.description}</p>
+        <img src={question.image} alt="postImage" />
       </section>
       <footer>
-        <h1>11 Respostas</h1>
-        <section>
-          <header>
-            <img src={imgProfile} alt="imageProfile" />
-            <strong>por Fulano</strong>
-          </header>
-          <p>Resposta para a pergunta</p>
-        </section>
-        <form>
-          <textarea placeholder="Responda essa duvida"></textarea>
+        <h1 onClick={() => setShowAnswers(!showAnswers)}>
+          {qtdAnswers === 0 ? (
+            "Seja o primeiro a responder"
+          ) : (
+            <>
+              {qtdAnswers} {qtdAnswers > 1 ? "Respostas" : "Resposta"}
+            </>
+          )}
+        </h1>
+        {showAnswers && (
+          <>
+            {answers.map((a) => (
+              <Answer answer={a} />
+            ))}
+          </>
+        )}
+
+        <form onSubmit={handleAddAnswer}>
+          <textarea
+            placeholder="Responda essa duvida"
+            onChange={(e) => setNewAnswer(e.target.value)}
+            required
+          ></textarea>
+          <button>Enviar</button>
         </form>
       </footer>
     </QuestionCard>
@@ -74,6 +129,7 @@ function Question({ question }) {
 }
 function Home() {
   const history = useHistory();
+
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
     const loadQuestions = async () => {
@@ -83,9 +139,9 @@ function Home() {
     };
     loadQuestions();
   }, []);
+
   const handleSingOut = () => {
     singOut();
-
     history.replace();
   };
   return (
@@ -102,7 +158,6 @@ function Home() {
           {questions.map((q) => (
             <Question question={q} />
           ))}
-
         </FeedContainer>
         <ActionsContainer>
           <button>Fazer uma pergunta</button>
